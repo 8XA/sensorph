b29896a81cc1da6ab2574e476ee046e733b71116
8XA <ochoa.ssa@gmail.com>
2022-09-27 10:41:47 -0700

    signal_reader muestra resultado de promedio en tiempo configurado

--- sensor.ino
+++ sensor.ino
@@ -2,13 +2,40 @@
 //--- INICIO DE VARIABLES MODIFICABLES ---
 
 //===================================
-//       VARIABLES PARA SET 1
+//       VARIABLES PARA SET 0
 //===================================
-byte pin_bomba_acida_set_1 = 13;
-byte pin_bomba_alcalina_set_1 = 12;
+byte pin_bomba_acida_set_0 = 13;
+byte pin_bomba_alcalina_set_0 = 12;
 
 //Debe ser un pin analógico
-byte pin_phimetro_set_1 = A0;
+byte pin_phimetro_set_0 = A0;
+//milisegundos entre cada lectura que va a promediarse
+int ms_para_muestreo_set_0 = 100;
+
+float segundos_bombeo_set_0 = 0.001;
+
+//Rango de PH en el que las bombas no operan
+float ph_maximo_set_0 = 7.5;
+float ph_minimo_set_0 = 7.0;
+//-----------------------------------
+//           CALIBRACIÓN
+//-----------------------------------
+float muestra1_ph_set_0 = 4.0;
+float muestra1_valor_set_0 = 1000;
+
+float muestra2_ph_set_0 = 10.0;
+float muestra2_valor_set_0 = 400;
+//===================================
+
+
+//===================================
+//       VARIABLES PARA SET 1
+//===================================
+byte pin_bomba_acida_set_1 = 11;
+byte pin_bomba_alcalina_set_1 = 10;
+
+//Debe ser un pin analógico
+byte pin_phimetro_set_1 = A1;
 //milisegundos entre cada lectura que va a promediarse
 int ms_para_muestreo_set_1 = 100;
 
@@ -31,11 +58,11 @@ float muestra2_valor_set_1 = 400;
 //===================================
 //       VARIABLES PARA SET 2
 //===================================
-byte pin_bomba_acida_set_2 = 11;
-byte pin_bomba_alcalina_set_2 = 10;
+byte pin_bomba_acida_set_2 = 9;
+byte pin_bomba_alcalina_set_2 = 8;
 
 //Debe ser un pin analógico
-byte pin_phimetro_set_2 = A1;
+byte pin_phimetro_set_2 = A2;
 //milisegundos entre cada lectura que va a promediarse
 int ms_para_muestreo_set_2 = 100;
 
@@ -54,38 +81,11 @@ float muestra2_ph_set_2 = 10.0;
 float muestra2_valor_set_2 = 400;
 //===================================
 
-
-//===================================
-//       VARIABLES PARA SET 3
-//===================================
-byte pin_bomba_acida_set_3 = 9;
-byte pin_bomba_alcalina_set_3 = 8;
-
-//Debe ser un pin analógico
-byte pin_phimetro_set_3 = A2;
-//milisegundos entre cada lectura que va a promediarse
-int ms_para_muestreo_set_3 = 100;
-
-float segundos_bombeo_set_3 = 0.001;
-
-//Rango de PH en el que las bombas no operan
-float ph_maximo_set_3 = 7.5;
-float ph_minimo_set_3 = 7.0;
-//-----------------------------------
-//           CALIBRACIÓN
-//-----------------------------------
-float muestra1_ph_set_3 = 4.0;
-float muestra1_valor_set_3 = 1000;
-
-float muestra2_ph_set_3 = 10.0;
-float muestra2_valor_set_3 = 400;
-//===================================
-
 //Segundos de espera entre cada lectura de bombas
 float segundos_espera = 3.0;
 //Set en turno cambia tras la ejecución de cada set.
 //El programa inicia con el set definido en esta variable
-int set_en_turno = 1;
+int set_en_turno = 0;
 
 //--- FIN DE VARIABLES MODIFICABLES ---
 
@@ -102,8 +102,19 @@ float muestra2_ph;
 float muestra2_valor;
 String set;
 
+float m;
+float offset;
+float pH;
+float sum_signal_value;
+float avg_signal_value;
+
 
 void setup() {
+    // Set 0
+    pinMode(pin_phimetro_set_0, INPUT);
+    pinMode(pin_bomba_acida_set_0, OUTPUT);
+    pinMode(pin_bomba_alcalina_set_0, OUTPUT);
+    
     // Set 1
     pinMode(pin_phimetro_set_1, INPUT);
     pinMode(pin_bomba_acida_set_1, OUTPUT);
@@ -113,23 +124,31 @@ void setup() {
     pinMode(pin_phimetro_set_2, INPUT);
     pinMode(pin_bomba_acida_set_2, OUTPUT);
     pinMode(pin_bomba_alcalina_set_2, OUTPUT);
-    
-    // Set 3
-    pinMode(pin_phimetro_set_3, INPUT);
-    pinMode(pin_bomba_acida_set_3, OUTPUT);
-    pinMode(pin_bomba_alcalina_set_3, OUTPUT);
 
     Serial.begin(9600);
 }
 
 
 void loop() {
-    if (set_en_turno > 3){
+    if (set_en_turno > 2){
         delay(segundos_espera * 1000);
-        set_en_turno = 1;
+        set_en_turno = 0;
     }
 
-    if (set_en_turno == 1) {
+    if (set_en_turno == 0) {
+        set = "SET 0";
+        pin_bomba_acida = pin_bomba_acida_set_0;
+        pin_bomba_alcalina = pin_bomba_alcalina_set_0;
+        pin_phimetro = pin_phimetro_set_0;
+        ms_para_muestreo = ms_para_muestreo_set_0;
+        segundos_bombeo = segundos_bombeo_set_0;
+        ph_maximo = ph_maximo_set_0;
+        ph_minimo = ph_minimo_set_0;
+        muestra1_ph = muestra1_ph_set_0;
+        muestra1_valor = muestra1_valor_set_0;
+        muestra2_ph = muestra2_ph_set_0;
+        muestra2_valor = muestra2_valor_set_0;
+    } else if (set_en_turno == 1) {
         set = "SET 1";
         pin_bomba_acida = pin_bomba_acida_set_1;
         pin_bomba_alcalina = pin_bomba_alcalina_set_1;
@@ -155,34 +174,25 @@ void loop() {
         muestra1_valor = muestra1_valor_set_2;
         muestra2_ph = muestra2_ph_set_2;
         muestra2_valor = muestra2_valor_set_2;
-    } else if (set_en_turno == 3) {
-        set = "SET 3";
-        pin_bomba_acida = pin_bomba_acida_set_3;
-        pin_bomba_alcalina = pin_bomba_alcalina_set_3;
-        pin_phimetro = pin_phimetro_set_3;
-        ms_para_muestreo = ms_para_muestreo_set_3;
-        segundos_bombeo = segundos_bombeo_set_3;
-        ph_maximo = ph_maximo_set_3;
-        ph_minimo = ph_minimo_set_3;
-        muestra1_ph = muestra1_ph_set_3;
-        muestra1_valor = muestra1_valor_set_3;
-        muestra2_ph = muestra2_ph_set_3;
-        muestra2_valor = muestra2_valor_set_3;
     }
 
-    digitalWrite(pin_bomba_acida, HIGH);
-    digitalWrite(pin_bomba_alcalina, HIGH);
+    digitalWrite(pin_bomba_acida_set_0, HIGH);
+    digitalWrite(pin_bomba_alcalina_set_0, HIGH);
+    digitalWrite(pin_bomba_acida_set_1, HIGH);
+    digitalWrite(pin_bomba_alcalina_set_1, HIGH);
+    digitalWrite(pin_bomba_acida_set_2, HIGH);
+    digitalWrite(pin_bomba_alcalina_set_2, HIGH);
 
-    float sum_signal_value = 0;
+    sum_signal_value = 0;
     for (int i=0; i < 10; i++) {
         sum_signal_value += analogRead(pin_phimetro);
         delay(ms_para_muestreo);
     }
-    float avg_signal_value = sum_signal_value/10;
+    avg_signal_value = sum_signal_value/10;
     
-    float m = (muestra2_ph - muestra1_ph)/(muestra2_valor - muestra1_valor);
-    float offset = muestra1_ph - m * muestra1_valor;
-    float pH = m * avg_signal_value + offset;
+    m = (muestra2_ph - muestra1_ph)/(muestra2_valor - muestra1_valor);
+    offset = muestra1_ph - m * muestra1_valor;
+    pH = m * avg_signal_value + offset;
 
     String estado = "neutro";
     if (pH < 7) {
--- signal_reader.ino
+++ signal_reader.ino
@@ -1,6 +1,8 @@
-
 byte pin_phimetro = A0;
-int ms_para_muestreo = 500;
+float ms_por_muestra = 10;
+float s_duracion_muestreo = 60.0;
+float suma_muestras = 0;
+float contador_de_muestras = 0;
 
 
 void setup() {
@@ -10,7 +12,14 @@ void setup() {
 
 
 void loop() {
-    Serial.println(String(analogRead(pin_phimetro)));
-    delay(ms_para_muestreo);
+    while (s_duracion_muestreo > 0) {
+        Serial.println(String(s_duracion_muestreo) + " " + String(analogRead(pin_phimetro)));
+        s_duracion_muestreo -= ms_por_muestra/1000;
+        suma_muestras += analogRead(pin_phimetro);
+        contador_de_muestras += 1;
+        delay(ms_por_muestra);
+    }
+    Serial.println("Val: " + String(suma_muestras/contador_de_muestras));
+    delay(60000);
 }
 
